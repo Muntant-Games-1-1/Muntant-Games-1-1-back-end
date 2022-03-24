35364dec591383323fe4db00c8ab7c4e0e646258
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-03-24 16:38:29 -0400

    add create lobby funtion

--- controllers/categories.js
+++ controllers/categories.js
@@ -4,13 +4,18 @@ function index(req, res) {
 	Category.find({})
 		.then(categories => res.json(categories))
 		.catch(err => {
-			console.log(err);
+			console.err(err);
 			res.status(500).json(err);
 		});
 }
 
-function create() {
-
+function create(req, res) {
+	Category.create(req.body)
+		.then(category => res.json(category))
+		.catch(err => {
+			console.err(err);
+			res.status(500).json(err);
+		});
 }
 
 export { index, create };
