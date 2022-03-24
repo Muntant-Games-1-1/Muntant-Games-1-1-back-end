3664371f7cdef4cd3611a10dc05d1ab34b8c2ef7
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-03-23 20:16:31 -0400

    complete category model

---
+++ models/category.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema(
+	{
+		name: String,
+		games: [{ type: Schema.Types.ObjectId, ref: "Game" }],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Category = mongoose.model("Category", categorySchema);
+
+export { Category };
