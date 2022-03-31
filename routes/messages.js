c41056d105e8d8a5a1adbb3403544f48eb2be5b6
Bestnogm1 <koffiarielhessou@gmail.com>
2022-03-31 13:19:06 -0400

    messages

--- routes/messages.js
+++ routes/messages.js
@@ -12,7 +12,7 @@ router.get("/:id", messagesCtrl.index);
 router.use(decodeUserFromToken);
 
 router.post("/", checkAuth, messagesCtrl.create);
-router.delete("/:id", checkAuth, messagesCtrl.delete);
+router.delete("/:id/me", checkAuth, messagesCtrl.delete);
 router.put("/:id", checkAuth, messagesCtrl.update)
 
 
